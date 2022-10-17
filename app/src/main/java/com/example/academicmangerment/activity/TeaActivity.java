3a007ed276aadac2b5839db9d3d3305034803a8c
xiong-make <2556709932@qq.com>
2022-10-17 20:37:15 +0800

    新建教师和管理员activity

---
+++ app/src/main/java/com/example/academicmangerment/activity/TeaActivity.java
@@ -0,0 +1,16 @@
+package com.example.academicmangerment.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.academicmangerment.R;
+
+public class TeaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tea);
+    }
+}
